b66a2dce7ef60616fe0b252293e2ef80c62b92e4
ZIJING, LIU <ae0672@student.jamk.fi>
2024-02-02 21:12:34 +0200

    Change camera script, and add some unnecessary components for Module 1

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -1,47 +1,74 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BasicMovement : MonoBehaviour
+namespace AE0672
 {
-    public float moveSpeed = 5f;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
 
-    void Update()
+    public class BasicMovement : MonoBehaviour
     {
+        public float moveSpeed = 5f;
+        [SerializeField] AudioSource hitSound;
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
-
-
-        transform.Translate(movement * moveSpeed * Time.deltaTime);
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Enemy")
+        private void Start()
         {
-            Debug.Log("Enemy hit");
+            hitSound = GetComponent<AudioSource>();
+        }
+        
 
-            StartCoroutine(DestroyEnemy(collision.gameObject));
+        void Update()
+        {
 
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+
+            Vector3 inputDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+
+
+            Vector3 cameraForward = Camera.main.transform.forward;
+            Vector3 cameraRight = Camera.main.transform.right;
+
+
+            cameraForward.y = 0f;
+            cameraRight.y = 0f;
+
+
+            cameraForward.Normalize();
+            cameraRight.Normalize();
+
+
+            Vector3 movementDirection = cameraForward * inputDirection.z + cameraRight * inputDirection.x;
+
+
+            transform.Translate(movementDirection * moveSpeed * Time.deltaTime, Space.World);
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag == "Enemy")
+            {
+                Debug.Log("Enemy hit");
+                hitSound.Play();
+
+                StartCoroutine(DestroyEnemy(collision.gameObject));
+
+            }
+        }
+
+        IEnumerator DestroyEnemy(GameObject enemy)
+        {
+
+            Renderer enemyRenderer = enemy.GetComponent<Renderer>();
+            if (enemyRenderer != null)
+            {
+                enemyRenderer.material.color = Color.red;
+            }
+
+
+            yield return new WaitForSeconds(2f);
+
+
+            Destroy(enemy);
         }
     }
 
-    IEnumerator DestroyEnemy(GameObject enemy)
-    {
-        
-        Renderer enemyRenderer = enemy.GetComponent<Renderer>();
-        if (enemyRenderer != null)
-        {
-            enemyRenderer.material.color = Color.red;
-        }
-
-      
-        yield return new WaitForSeconds(2f);
-
-        
-        Destroy(enemy);
-    }
 }
